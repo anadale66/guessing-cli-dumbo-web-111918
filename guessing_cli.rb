53d7275456e3b7e1823f80ed706259c06dc04f4b
Roman Pirag <mr.anadale@yahoo.com>
2018-11-16 01:19:29 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -14,7 +14,7 @@ def run_guessing_game
   when "exit"
     puts "Goodbye"
   when rand_num
-    puts "You guesses the correct number!"
+    puts "You guessed the correct number!"
     run_guessing_game 
   else
     puts "The computer guessed #{rand_num}."
